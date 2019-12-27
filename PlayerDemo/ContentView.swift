7319de376ba89bcfb12d8ab065ede99e5ed93d7e
yz <liuyz@powerinfo.net>
2019-12-27 17:07:03 +0800

    init
    init

---
+++ PlayerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayerDemo
+//
+//  Created by Easer Liu on 2019/12/27.
+//  Copyright © 2019 EasyGoing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
